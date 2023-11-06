c0f8f86b8e52bda4a67cc01c148e3cac80d54c46
DawnHavoc <sarnavadas2@gmail.com>
2023-11-06 21:35:14 +0530

    commit

--- parent/component/model_evaluation.py
+++ parent/component/model_evaluation.py
@@ -1,25 +1,15 @@
-# import ast
-# train_data['DESCRIPTION'] = train_data['DESCRIPTION'].apply(lambda x: ast.literal_eval(x))
-# processed_data['TF-IDF'] = processed_data['TF-IDF'].apply(lambda x: np.array(eval(x)[0]))
 import os
 import pandas as pd
 import pathlib 
-import numpy as np
 
-
-# from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier
-
-
 from sklearn.model_selection import train_test_split
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-
 def gb_model(X_train,y_train,X_test,y_test):
-        # Create and train the Gradient Boosting model
+    # Create and train the Gradient Boosting model
     gb_model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
     gb_model.fit(X_train, y_train)
 
@@ -34,7 +24,7 @@ def gb_model(X_train,y_train,X_test,y_test):
 
 def rf_model(X_train,y_train,X_test,y_test):
     # Create a Random Forest classifier
-    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=0)  # You can adjust the number of estimators as needed
+    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=0) 
 
     # Fit the classifier to the training data
     rf_classifier.fit(X_train, y_train)
@@ -44,7 +34,6 @@ def rf_model(X_train,y_train,X_test,y_test):
 
     # Calculate and print accuracy
     rf_accuracy = accuracy_score(y_test, rf_predictions)
-   
     print(f"Random Forest Accuracy: {rf_accuracy * 100:.2f}%")
 
 def lr_model(X_train,y_train,X_test,y_test):
@@ -55,8 +44,7 @@ def lr_model(X_train,y_train,X_test,y_test):
 
     predictions = lr.predict(X_test)
 
-   
-      # Calculate accuracy 
+    # Calculate accuracy 
     accuracy = accuracy_score(y_test, predictions)
     
     print(f"Logistic Regression Accuracy: {accuracy * 100:.2f}%")
@@ -79,19 +67,16 @@ def getfile():
 def main():
     train_set_file=getfile()
    
-    # Select relevant features
+   
     X=pd.read_csv(train_set_file)
     # Target variable
     y = X['Exited']
 
-    # Exclude the 'Exited' column from the features
+    # Exclude the 'Exited' column from the features to select relevant features
     X = X.drop(columns=['Exited'])
     
     # Split the data into train and test sets 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-     
-  
-    
    
     lr_model(X_train,y_train,X_test,y_test)
     gb_model(X_train,y_train,X_test,y_test)
